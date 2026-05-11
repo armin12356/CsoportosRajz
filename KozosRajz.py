89d84819d41da28851d2c99e80317cdbbc986c5a
Tofalvi Plosz Armin <ploszarmin092@gmail.com>
2026-05-11 08:56:01 +0200

    lombok

--- KozosRajz.py
+++ KozosRajz.py
@@ -1,3 +1,19 @@
 import turtle
+turtle.speed(0)
+turtle.color("green")
+turtle.begin_fill()
+i=60
+while i<120:
 
-#fa alja, es hatter
+    turtle.circle(60)
+    turtle.left(i)
+    turtle.circle(60)
+    turtle.right(i+60)
+    turtle.circle(60)
+    i+=60
+turtle.fillcolor("green")
+turtle.end_fill()
+turtle.done()
+
+
+#fa lombja
