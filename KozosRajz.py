4c05ae9fc32ce66c9558d57f001f084db180c50d
Zskris <zsinkaikristof@gmail.com>
2026-05-11 08:55:45 +0200

    fa alja es hatter

--- KozosRajz.py
+++ KozosRajz.py
@@ -1,3 +1,17 @@
 import turtle
 
-#fa alja, es hatter
+turtle.bgcolor("lightblue")
+turtle.speed(0)
+turtle.color("brown")
+turtle.begin_fill()
+i=0
+while i<2:
+    turtle.forward(100)
+    turtle.left(-90)
+    turtle.forward(300)
+    turtle.left(-90)
+    i+=1
+turtle.fillcolor("brown")
+turtle.end_fill()
+
+turtle.done()
